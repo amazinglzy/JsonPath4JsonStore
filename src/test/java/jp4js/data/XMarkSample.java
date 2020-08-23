76cd15cd31a961879e57041553f42c9225cdca58
amazinglzy <1075749478@qq.com>
2020-08-23 11:26:02 +0800

    Update XMarkSample TestSuite

--- src/test/java/jp4js/data/XMarkSample.java
+++ src/test/java/jp4js/data/XMarkSample.java
@@ -1,5 +1,9 @@
 package jp4js.data;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class XMarkSample extends BaseDataSuite {
     @Override
     public String[] querySet() {
@@ -14,8 +18,9 @@ public class XMarkSample extends BaseDataSuite {
     @Override
     public String[] res() {
         return new String[]{
-            "([(\"45.13\")])",
-            "((\"United States\"))",
+            "([(\"62.07\"), (\"155.28\"), (\"451.27\"), (\"61.60\"), (\"19.59\"), "+
+                "(\"9.41\"), (\"21.75\"), (\"89.93\"), (\"194.61\"), (\"151.43\")])",
+            "",
             "([(\"Johnathan Senouci\")])",
             "([(\"542.00\")])"
         };
@@ -23,120 +28,13 @@ public class XMarkSample extends BaseDataSuite {
     
     @Override
     public String data() {
-        return 
-            "    {" +
-            "        \"region\": {" +
-            "            \"<region_name>\": [" +
-            "                {" +
-            "                    \"id\": \"item0\"," +
-            "                    \"location\": \"United States\"," +
-            "                    \"quantity\": 1," +
-            "                    \"name\": \"duteous nine eighteen\"," +
-            "                    \"payment\": \"Creditcard\"," +
-            "                    \"description\": {" +
-            "                    \"parlist\": [" +
-            "                        \"page rous lady idle authority capt professes stabs monster petition heave humbly removes rescue runs shady peace most piteous worser oak assembly holes patience but malice whoreson mirrors master tenants smocks yielded<keyword> officer embrace such fears distinction attires </keyword>\"," +
-            "                        \"shepherd noble supposed dotage humble servilius bitch theirs venus dismal wounds gum merely raise red breaks earth god folds closet captain dying reek\"" +
-            "                    ]" +
-            "                    }," +
-            "                    \"shipping\": \"Will ship internationally, See description for charges\"," +
-            "                    \"incategory\": [" +
-            "                        \"category67\"," +
-            "                        \"category51\"," +
-            "                        \"category122\"," +
-            "                        \"category97\"," +
-            "                        \"category1\"" +
-            "                    ]," +
-            "                    \"mailbox\": [" +
-            "                    {" +
-            "                        \"from\": \"Libero Rive mailto:Rive@hitachi.com\"," +
-            "                        \"to\": \"Benedikte Glew mailto:Glew@sds.no\"," +
-            "                        \"date\": \"07/05/2000\"," +
-            "                        \"text\": \"virgin preventions half logotype weapons granted factious already carved fretted impress pestilent<keyword> girdles deserts flood george nobility reprieve </keyword>discomfort sinful conceiv corn preventions greatly suit observe sinews enforcement<emph> armed </emph>gold gazing set almost catesby turned servilius cook doublet preventions shrunk smooth great choice enemy disguis tender might deceit ros dreadful stabbing fold unjustly ruffian life hamlet containing leaves\"" +
-            "                    }" +
-            "                    ]" +
-            "                }" +
-            "            ]" +
-            "        }," +
-            "        \"categories\": [{\"name\": \"xx\", \"description\": \"xxx\"}]," +
-            "        \"catgraph\": [{\"from\": \"category01\", \"to\": \"category02\"}]," +
-            "        \"people\": [" +
-            "            {" +
-            "                \"id\": \"person0\"," +
-            "                \"name\": \"Johnathan Senouci\"," +
-            "                \"emailaddress\": \"mailto:Senouci@ualberta.ca\"," +
-            "                \"address\": {" +
-            "                    \"street\": \"55 Chubarov St\"," +
-            "                    \"city\": \"Caracas\"," +
-            "                    \"country\": \"Seychelles\"," +
-            "                    \"zipcode\": \"5\"" +
-            "                }," +
-            "                \"profile\": {" +
-            "                    \"business\": \"Yes\"," +
-            "                    \"gender\": \"male\"," +
-            "                    \"age\": \"18\"," +
-            "                    \"interest\": [" +
-            "                    \"category72\"," +
-            "                    \"category79\"," +
-            "                    \"category77\"" +
-            "                    ]" +
-            "                }," +
-            "                \"watches\": [" +
-            "                    \"open_auction1497\"" +
-            "                ]" +
-            "            }" +
-            "        ]," +
-            "        \"open_auctions\": [" +
-            "            {" +
-            "                \"id\": \"open_auction0\"," +
-            "                \"initial\": \"89.88\"," +
-            "                \"current\": \"158.88\"," +
-            "                \"quantity\": \"1\"," +
-            "                \"type\": \"Regular\"," +
-            "                \"reserve\": \"542.00\"," +
-            "                \"bidder\": [" +
-            "                    {" +
-            "                        \"date\": \"05/06/1999\"," +
-            "                        \"time\": \"06:54:33\"," +
-            "                        \"personref\": \"person2520\"," +
-            "                        \"increase\": \"57.00\"" +
-            "                    }," +
-            "                    {" +
-            "                        \"date\": \"11/23/2001\"," +
-            "                        \"time\": \"00:08:50\"," +
-            "                        \"personref\": \"person8\"," +
-            "                        \"increase\": \"12.00\"" +
-            "                    }" +
-            "                ]," +
-            "                \"itemref\": \"item0\"," +
-            "                \"seller\": \"person2323\"," +
-            "                \"interval\": {" +
-            "                    \"start\": \"03/10/2001\"," +
-            "                    \"end\": \"04/05/1998\"" +
-            "                }" +
-            "            }" +
-            "        ]," +
-            "        \"closed_auctions\": [" +
-            "            {" +
-            "                \"price\": \"45.13\"," +
-            "                \"date\": \"05/19/1999\"," +
-            "                \"quantity\": \"1\"," +
-            "                \"type\": \"Regular\"," +
-            "                \"seller\": \"person1536\"," +
-            "                \"buyer\": \"person1773\"," +
-            "                \"itemref\": \"item1\"," +
-            "                \"annotation\": {" +
-            "                    \"author\": \"person2251\"," +
-            "                    \"description\": {" +
-            "                    \"parlist\": [" +
-            "                        \"communicate have fond vienna stain gentles marian dat here apology fret ail unexpected rowland parolles renew\"," +
-            "                        \"<bold> safe ducats inclining square infringe wits cock </bold>mine returns comply chaste guide affects troilus envious bless swearing entertain parching quiver loathes dissembling faiths last makes haste offence fortune humphrey came weaker dove mood<emph> since fawn clawed speaking employ gibes smooth </emph>twenty twelvemonth lammas mild tush justice kindled eros forehead hours modern reason thyself fulfill itself pet eldest distant clergy natures educational players wretches discharg coats ditch<bold> methoughts </bold>abominable had intent prophet knots durst scene hire early thought cousins kiss sums guests fares yonder understanding majesties burgundy trusty buys slay helmets galled bias gone bowls peasant wildness detect\"" +
-            "                    ]" +
-            "                    }," +
-            "                    \"happiness\": \"8\"" +
-            "                }" +
-            "            }" +
-            "        ]" +
-            "    }";
+        ClassLoader loader = getClass().getClassLoader();
+        File xmarkFile = new File(loader.getResource("xmark_sample.json").getFile());
+        try {
+            return new String(Files.readAllBytes(xmarkFile.toPath()));
+        } catch (IOException e) {
+            assert(false);
+        }
+        return null;
     }
 }
